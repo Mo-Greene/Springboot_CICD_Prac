1f0fb8c3ef0ffa0842f86be22120d6265714e3a4
Mo-Greene <13blueboy13@daum.net>
2022-07-22 19:58:38 +0900

    키워드  - 기능, 단위  #
    변경사항 :

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,31 +1,19 @@
-PROJECT=CiCd0722
-REPOSITORY=/home/ubuntu/drunken-wizard
+REPOSITORY=/home/ubuntu/drunken-wizard/build/libs
+cd $REPOSITORY
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
+APP_NAME=drunken-wizard
+JAR_NAME=$(ls $REPOSITORY/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/$JAR_NAME
 
-CURRENT_PID=$(pgrep -fl $PROJECT | grep jar | awk '{print $1}')
-
-echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+CURRENT_PID=$(pgrep -f $APP_NAME)
+if [ -z $CURRENT_PID ] #2
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 "$CURRENT_PID"
+  sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> 새 애플리케이션 배포"
-
-JAR_NAME=$(find $REPOSITORY/*.jar | tail -n 1)
-
-echo "> JAR NAME: $JAR_NAME"
-
-echo "> $JAR_NAME 에 실행권한 추가"
-
-chmod +x "$JAR_NAME"
-
-echo "> $JAR_NAME 실행"
-
-# 실행
-nohup java -Duser.timezone=KST -jar "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
+echo "> $JAR_PATH 배포" #3
+nohup java -jar /home/ubuntu/drunken-wizard/build/libs/makeCake-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
