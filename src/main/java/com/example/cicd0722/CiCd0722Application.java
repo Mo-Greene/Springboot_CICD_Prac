f2cadeff2a5d7df40efb4199f6f43a82b6e15aaf
Mo-Greene <13blueboy13@daum.net>
2022-07-22 11:00:25 +0900

    Initial commit

---
+++ src/main/java/com/example/cicd0722/CiCd0722Application.java
@@ -0,0 +1,13 @@
+package com.example.cicd0722;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCd0722Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiCd0722Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd0722/CiCd0722ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd0722;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCd0722ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
