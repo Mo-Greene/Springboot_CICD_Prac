1da7d0296ffbfdf785abbac14f2bca6721148d1e
Mo-Greene <13blueboy13@daum.net>
2022-07-22 23:01:36 +0900

    deploy shit

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,4 +16,4 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/ubuntu/drunken-wizard/CiCd0722-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ubuntu/drunken-wizard/deploy/CiCd0722-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
